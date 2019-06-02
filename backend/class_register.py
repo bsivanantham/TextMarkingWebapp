5b56dfb6d0fad5dd45775acaf75f57772fea5e74
Balavivek Sivanantham <balavivek107@gmail.com>
2019-06-02 23:45:18 +0530

    Add files via upload

---
+++ backend/class_register.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+AVAILABLE_MODELS = {}
+
+
+def register_api(name):
+    def decorator(cls):
+        print("Found API {} with name {}".format(
+            cls, name))
+        AVAILABLE_MODELS[name] = cls
+        return cls
+    return decorator
